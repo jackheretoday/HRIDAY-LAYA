c12b5301a2975980ea18f706e19f6e03be7e2a77
Jay Kshirsagar <150058941+jackheretoday@users.noreply.github.com>
2025-03-19 19:11:02 +0530

    Update urls.py

--- Hriday/urls.py
+++ Hriday/urls.py
@@ -1,28 +1,27 @@
-"""Hriday URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from . import views
-from Hriday.views import video_call
+
 urlpatterns = [
-    path('adminlogin.html', admin.site.urls),
-    path('home.html',views.home,name='home'),
-    path('patient.html',views.patient,name='patient'),  
-    path('adminlogin.html', views.adminlogin, name='adminlogin'),  # Admin login page
-    path('doctor.html', views.doctor, name='doctor'), 
+    # Django Admin Panel
+    path('http://127.0.0.1:8000/admin/', admin.site.urls),
+
+    # Webpages
+    path('', views.home, name='home'),  # Default home page
+    path('http://127.0.0.1:8000/admin/', admin.site.urls),
+    path('patient.html', views.patient, name='patient'),
+    path('adminlogin.html', views.adminlogin, name='adminlogin'),
+    path('doctor.html', views.doctor, name='doctor'),
     path('video_call.html', views.video_call, name='video_call'),
+    path('home.html', views.home, name='home'),
+    path('health_data.html', views.health_data, name='health_data'),  # Add this
+    path('recommendation_dashboard.html', views.recommendation_dashboard, name='recommendation_dashboard'), 
+    path('recomendation.html', views.food_recommendation, name='food_recommendation'),  # Add this
+    path('location_recomendation.html', views.location_recommendation, name='location_recommendation'),  # Add this
+    path('patient_signup.html', views.patient_signup, name='patient_signup'),  # Add this
+    path('doctor_signup.html', views.doctor_signup, name='doctor_signup'),  # Add this
+    # Prediction Routes
+    path('prediction.html', views.predict, name='predict'),  # Form-based prediction
+    path('api/predict/', views.predict_heart_disease, name='predict_heart_disease'),  # API endpoint
 ]
 
